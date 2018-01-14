e60eff095827c37aa60c751bdd2ab7f63e67a00c
kadirayk <kadiray.k@hotmail.com>
2018-01-14 19:13:14 +0100

    enabled webapp test

--- src/test/java/app/ApplicationTest.java
+++ src/test/java/app/ApplicationTest.java
@@ -1,37 +1,36 @@
 
 package app;
 
-//import static org.hamcrest.Matchers.containsString;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(properties = "logging.level.org.springframework.web=DEBUG")
-//@AutoConfigureMockMvc
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = "logging.level.org.springframework.web=DEBUG")
+@AutoConfigureMockMvc
 public class ApplicationTest {
-//
-//	@Autowired
-//	private MockMvc mockMvc;
-//
-//	@Test
-//	public void rendersForm() throws Exception {
-//		mockMvc.perform(get("/greeting")).andExpect(content().string(containsString("Form")));
-//	}
-//
-//	@Test
-//	public void submitsForm() throws Exception {
-//		mockMvc.perform(post("/greeting").param("id", "12345").param("content", "Hello"))
-//				.andExpect(content().string(containsString("Result")))
-//				.andExpect(content().string(containsString("id: 12345")));
-//	}
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void rendersForm() throws Exception {
+		mockMvc.perform(get("/init")).andExpect(content().string(containsString("Input")));
+	}
+
+	@Test
+	public void submitsForm() throws Exception {
+		mockMvc.perform(post("/init").param("content", "I want a Machine Learning system."))
+				.andExpect(content().string(containsString("Machine Learning")));
+	}
 
 }
