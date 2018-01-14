3e2022a19cd3f62a6436e282bea98f05c8da973e
kadirayk <kadiray.k@hotmail.com>
2018-01-14 20:01:19 +0100

    added tests for page transitions

--- src/test/java/app/ApplicationTest.java
+++ src/test/java/app/ApplicationTest.java
@@ -6,14 +6,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringRunner.class)
 @SpringBootTest(properties = "logging.level.org.springframework.web=DEBUG")
 @AutoConfigureMockMvc
@@ -23,14 +26,24 @@ public class ApplicationTest {
 	private MockMvc mockMvc;
 
 	@Test
-	public void rendersForm() throws Exception {
+	public void aRendersForm() throws Exception {
 		mockMvc.perform(get("/init")).andExpect(content().string(containsString("Input")));
 	}
 
 	@Test
-	public void submitsForm() throws Exception {
+	public void bSubmitsForm() throws Exception {
 		mockMvc.perform(post("/init").param("content", "I want a Machine Learning system."))
 				.andExpect(content().string(containsString("Machine Learning")));
+	}
+
+	@Test
+	public void cNextOnForm() throws Exception {
+		mockMvc.perform(get("/next")).andExpect(content().string(containsString("What type of learning")));
+	}
+
+	@Test
+	public void dPrevOnForm() throws Exception {
+		mockMvc.perform(get("/prev")).andExpect(content().string(containsString("What is the name of")));
 	}
 
 }
