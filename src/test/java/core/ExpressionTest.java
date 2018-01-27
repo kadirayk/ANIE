f332bb5f91b44f30f68b20e381c109b02925438c
kadirayk <kadiray.k@hotmail.com>
2018-01-27 14:44:10 +0100

    enabled expressiontests

--- src/test/java/core/ExpressionTest.java
+++ src/test/java/core/ExpressionTest.java
@@ -7,28 +7,28 @@ import org.junit.Test;
 
 public class ExpressionTest {
 
-//	@Test
+	@Test
 	public void Test1() {
 		String exp = "a=a & (b=c|(c=c))";
 		ExpressionEvaluator ev = new ExpressionEvaluator(exp);
 		assertTrue(ev.evaluateExpression());
 	}
 
-//	@Test
+	@Test
 	public void Test2() {
 		String exp = "a=b | (b=c | (c=c & d=d)))";
 		ExpressionEvaluator ev = new ExpressionEvaluator(exp);
 		assertTrue(ev.evaluateExpression());
 	}
 
-//	@Test
+	@Test
 	public void TestFalse() {
 		String exp = "a=a & (b=c | (c=c & d=d)) & f=f & g=h )";
 		ExpressionEvaluator ev = new ExpressionEvaluator(exp);
 		assertFalse(ev.evaluateExpression());
 	}
 
-//	@Test
+	@Test
 	public void TestTrue3() {
 		String exp = "a=a & (b=c | (c=c & d=d)) & f=f & g=g )";
 		ExpressionEvaluator ev = new ExpressionEvaluator(exp);
